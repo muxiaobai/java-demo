2d4e012c279b789486694dbdee75346423929b6e
muxiaobai <zpf12345678910@gmail.com>
2020-11-13 11:12:50 +0800

    区间  nSum问题

---
+++ test-java-demo/src/main/java/io/github/muxiaobai/labuladong/iii.java
@@ -0,0 +1,178 @@
+package io.github.muxiaobai.labuladong;
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 区间问题
+ * 1288  56  986
+ **/
+public class iii {
+
+    public static void main(String[] args) {
+//        System.out.println(removeCoveredIntervals(new int[][]{{1, 4}, {3, 6}, {2, 8},{3,5},{6,7}}));
+//        printf(merge(new int[][]{{1, 4}, {3, 4}, {9, 23}, {8, 8}, {2, 8}}));
+//        printf(merge(new int[][]{{2, 3}, {5, 5}, {2, 2}, {3, 4}, {4,4},{3, 4}}));
+        printf(intervalIntersection(new int[][]{{0, 2}, {5, 10}, {13, 23}, {24, 25}}, new int[][]{{1, 5}, {8, 12},
+                {15, 24}, {25, 26}}));
+
+//        System.out.println(countRangeSum(new int[]{1, 43, 62, 83,56,7},1,1));
+
+    }
+
+    /**
+     * 返回 没有被覆盖的区间个数
+     * 1288
+     *
+     * @param intervals
+     * @return
+     */
+
+    public static int removeCoveredIntervals(int[][] intervals) {
+        Arrays.sort(intervals, (x, y) -> {
+            if (x[0] == y[0]) {
+                return y[1] - x[1];
+            } else {
+                return x[0] - y[0];
+            }
+        });
+        printf(intervals);
+        int left = intervals[0][0];
+        int right = intervals[0][1];
+        int count = 0;
+        for (int i = 1; i < intervals.length; i++) {
+            // 下一个区间在内部
+            if (left <= intervals[i][0] && intervals[i][1] <= right) {
+                count++;
+                continue;
+            }
+            // 存在重合区间
+            if (left <= intervals[i][0] && intervals[i][0] <= right && right < intervals[i][1]) {
+                right = intervals[i][1];
+                continue;
+            }
+            // 无关
+            if (left <= intervals[i][0] && right <= intervals[i][0]) {
+                left = intervals[i][0];
+                right = intervals[i][1];
+                continue;
+            }
+
+        }
+        return intervals.length - count;
+    }
+
+    public static void printf(int[][] intervals) {
+        for (int i = 0; i < intervals.length; i++) {
+            System.out.println("["+intervals[i][0] + ", " + intervals[i][1]+"]");
+        }
+    }
+
+    /**
+     * 返回合并区间
+     * 56
+     *
+     * @param intervals
+     * @return
+     */
+    public static int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals, (x, y) -> {
+            if (x[0] == y[0]) {
+                return y[1] - x[1];
+            } else {
+                return x[0] - y[0];
+            }
+        });
+//        printf(intervals);
+        List<int[]> merged = new ArrayList<int[]>();
+        int left = 0;
+        int right = 0;
+        for (int i = 0; i < intervals.length; i++) {
+
+            // 下一个区间在内部
+            if (left <= intervals[i][0] && intervals[i][1] <= right) {
+            }
+            // 存在重合区间
+            if (left <= intervals[i][0] && intervals[i][0] <= right && right < intervals[i][1]) {
+                right = intervals[i][1];
+            }
+            // 无关
+            if (right < intervals[i][0]) {
+                //第一个区间不动
+                if (i != 0) {
+                    merged.add(new int[]{left, right});
+                }
+                left = intervals[i][0];
+                right = intervals[i][1];
+            }
+            // 添加最后的一个区间
+            if (i == intervals.length - 1) {
+                merged.add(new int[]{left, right});
+            }
+
+        }
+        return merged.toArray(new int[merged.size()][]);
+    }
+
+    /**
+     * 986  区间求并集
+     * -------  -------   ---------     ----------
+     * --   ---       -  -----    -----------
+     * --   --        -   --      -     -----
+     * 求区间并集
+     * @param A
+     * @param B
+     * @return
+     */
+    public static int[][] intervalIntersection(int[][] A, int[][] B) {
+//        Arrays.sort(A, (x, y) -> {
+//            if (x[0] == y[0]) {
+//                return y[1] - x[1];
+//            } else {
+//                return x[0] - y[0];
+//            }
+//        });
+//        Arrays.sort(B, (x, y) -> {
+//            if (x[0] == y[0]) {
+//                return y[1] - x[1];
+//            } else {
+//                return x[0] - y[0];
+//            }
+//        });
+        List<int[]> merged = new ArrayList<int[]>();
+        int i = 0;
+        int j = 0;
+        while (i < A.length && j < B.length) {
+            // 两个区间比较  左侧取最大，右侧取最小，
+            // 根据右侧大小，进行位置移动
+
+            //两个区间存在交集 相互比大小
+            if (A[i][1] >= B[j][0] && B[j][1] >= A[i][0]) {
+                // 取开始区间的最大值，取结束区间的最小值
+                merged.add(new int[]{Math.max(A[i][0], B[j][0]), Math.min(A[i][1], B[j][1])});
+            }
+            // 更新位置  B大 需要调大A的位置
+            if (A[i][1] <= B[j][1]) {
+                i++;
+            } else {
+                j++;
+            }
+        }
+        return merged.toArray(new int[merged.size()][]);
+    }
+
+    /**
+     * 327
+     *
+     * @param nums
+     * @param lower
+     * @param upper
+     * @return
+     */
+    public static int countRangeSum(int[] nums, int lower, int upper) {
+        return 0;
+    }
+}
+
---
+++ test-java-demo/src/main/java/io/github/muxiaobai/labuladong/jjj.java
@@ -0,0 +1,310 @@
+package io.github.muxiaobai.labuladong;
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * nSum  和问题
+ **/
+public class jjj {
+
+    public static void main(String[] args) {
+//        System.out.println(Arrays.toString(twoSum(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9}, 10)));
+//        System.out.println(Arrays.toString(twoSum2(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9}, 10)));
+//        System.out.println(Arrays.toString(twoSum3(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9}, 10)));
+//        iii.printf(twoSum4(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9}, 10));
+//        iii.printf(twoSum5(new int[]{1,1, 2, 3, 4, 5, 6, 7, 8, 9}, 10));
+
+//        iii.printf(twoSum5(new int[]{-1,-1, -2, -3, -4, -5, -6, -7, -8, -9, 1, 2, 3, 4, 5, 6, 7, 8, 9},0,-1));
+//        System.out.println(threeSum(new int[]{-1,-1, -2, -3, -4, -5, -6, -7, -8, -9, 1, 2, 3, 4, 5, 6, 7, 8, 9}));
+//        System.out.println(threeSum(new int[]{-1, 0, 1, 2, -1, -4}));
+//        System.out.println(fourSum(new int[]{-1, -1, -2, -3, -4, -5, -6, -7, -8, -9, 1, 2, 3, 4, 5, 6, 7, 8, 9}, 0));
+
+
+        System.out.println(nSum(new int[]{-1, -1, -2, -3, -4, -5, -6, -7, -8, -9, 1, 2, 3, 4, 5, 6, 7, 8, 9}, 4,0));
+
+
+    }
+
+    /**
+     * 1  两数和
+     *
+     * @param nums
+     * @return
+     */
+    public static int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return null;
+    }
+
+    /**
+     * 1  两数和
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public static int[] twoSum2(int[] nums, int target) {
+        HashMap<Integer, Integer> hashMap = new HashMap();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (hashMap.containsKey(target - nums[i])) {
+                return new int[]{hashMap.get(target - nums[i]), i};
+            }
+            hashMap.put(nums[i], i);
+        }
+
+        return null;
+    }
+
+    /**
+     * 1  两数和
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public static int[] twoSum3(int[] nums, int target) {
+        Arrays.sort(nums);
+        int left = 0;
+        int right = nums.length - 1;
+        while (left < right) {
+            int sum = nums[left] + nums[right];
+            if (sum < target) {
+                left++;
+            } else if (sum > target) {
+                right--;
+            } else if (sum == target) {
+                return new int[]{left, right};
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * 1  两数和
+     * 返回所有满足条件的数字
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public static int[][] twoSum4(int[] nums, int target) {
+        HashMap<Integer, Integer> hashMap = new HashMap();
+        List<int[]> merged = new ArrayList<int[]>();
+        for (int i = 0; i < nums.length; i++) {
+            if (hashMap.containsKey(target - nums[i])) {
+                merged.add(new int[]{target - nums[i], nums[i]});
+            }
+            hashMap.put(nums[i], i);
+        }
+        return merged.toArray(new int[merged.size()][]);
+    }
+
+    /**
+     * 1  两数和
+     * 收尾指针
+     * 左右横跳
+     * @param nums
+     * @param  start
+     * @param target
+     * @return
+     */
+    public static List<List<Integer>> twoSum5(int[] nums, int start, int target) {
+        Arrays.sort(nums);
+        List<List<Integer>> merged = new ArrayList<>();
+        int left = start;
+        int right = nums.length - 1;
+        while (left < right) {
+            int sum = nums[left] + nums[right];
+            int a = nums[left], b = nums[right];
+            if (sum < target) {
+                while (left < right && nums[left] == a) {
+                    left++;
+                }
+            } else if (sum > target) {
+                while (left < right && nums[right] == b) {
+                    right--;
+                }
+            } else if (sum == target) {
+                List<Integer> list = new ArrayList<>();
+                list.add(nums[left]);
+                list.add(nums[right]);
+                merged.add(list);
+                while (left < right && nums[left] == a) {
+                    left++;
+                }
+                while (left < right && nums[right] == b) {
+                    right--;
+                }
+            }
+        }
+        return merged;
+    }
+
+    /**
+     * 15 三数和
+     * a+ b+ c =0
+     *
+     * @param nums
+     * @return
+     */
+    public static List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        int target = 0;
+
+        List<List<Integer>> merged = new ArrayList<List<Integer>>();
+        for (int i = 0; i < nums.length; i++) {
+            List<List<Integer>> ttt = twoSum5(nums, i + 1, target - nums[i]);
+            for (int j = 0; j < ttt.size(); j++) {
+                List<Integer> list = ttt.get(j);
+                list.add(nums[i]);
+                merged.add(list);
+            }
+            // 跳过第一个数字重复的情况，否则会出现重复结果
+            while (i < nums.length - 1 && nums[i] == nums[i + 1]) {
+                i++;
+            }
+        }
+        return merged;
+
+    }
+
+    /**
+     * 15 三数和
+     * a+ b+ c =0
+     *
+     * @param nums
+     * @return
+     */
+    public static List<List<Integer>> threeSum2(int[] nums, int left, int target) {
+        Arrays.sort(nums);
+        List<List<Integer>> merged = new ArrayList<List<Integer>>();
+        for (int i = left; i < nums.length; i++) {
+            List<List<Integer>> ttt = twoSum5(nums, i + 1, target - nums[i]);
+            for (int j = 0; j < ttt.size(); j++) {
+                List<Integer> list =ttt.get(j);
+                list.add(nums[i]);
+                merged.add(list);
+            }
+            // 跳过第一个数字重复的情况，否则会出现重复结果
+            while (i < nums.length - 1 && nums[i] == nums[i + 1]) {
+                i++;
+            }
+        }
+        return merged;
+
+    }
+
+    /**
+     * 18 四数和
+     * a+ b+ c +d =target
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public static List<List<Integer>> fourSum(int[] nums, int target) {
+        Arrays.sort(nums);
+        List<List<Integer>> merged = new ArrayList<List<Integer>>();
+        for (int i = 0; i < nums.length; i++) {
+            List<List<Integer>> ttt = threeSum2(nums, i + 1, target - nums[i]);
+            for (int j = 0; j < ttt.size(); j++) {
+                List<Integer> list = ttt.get(j);
+                list.add(nums[i]);
+                merged.add(list);
+            }
+            // 跳过第一个数字重复的情况，否则会出现重复结果
+            while (i < nums.length - 1 && nums[i] == nums[i + 1]) {
+                i++;
+            }
+        }
+        return merged;
+    }
+
+    /**
+     * 推广 nSum
+     *
+     * @param nums 数组
+     * @param n 几位数之和
+     * @param target 和为多少
+     * @return
+     */
+
+    public static List<List<Integer>> nSum(int[] nums, int n, int target) {
+        Arrays.sort(nums);
+        return nSum2(nums, n, 0, target);
+    }
+
+    /**
+     * nSum n数和
+     *
+     * 2 为第一个
+     * 之后的递归 回到aaa.java 的问题
+     * @param nums 数组
+     * @param n  几位数之和
+     * @param start 从多少开始算
+     * @param target 和为多少
+     * @return List
+     */
+    public static List<List<Integer>> nSum2(int[] nums, int n, int start, int target) {
+        List<List<Integer>> merged = new ArrayList<List<Integer>>();
+        if (n < 2 || nums.length < n) {
+            return merged;
+        }
+        if (n == 2) {
+            int left = start;
+            int right = nums.length - 1;
+            while (left < right) {
+                int sum = nums[left] + nums[right];
+                int a = nums[left], b = nums[right];
+                if (sum < target) {
+                    while (left < right && nums[left] == a) {
+                        left++;
+                    }
+                } else if (sum > target) {
+                    while (left < right && nums[right] == b) {
+                        right--;
+                    }
+                } else if (sum == target) {
+                    List<Integer> list = new ArrayList<>();
+                    list.add(nums[left]);
+                    list.add(nums[right]);
+                    merged.add(list);
+                    while (left < right && nums[left] == a) {
+                        left++;
+                    }
+                    while (left < right && nums[right] == b) {
+                        right--;
+                    }
+                }
+            }
+            return merged;
+        } else {
+            for (int i = 0; i < nums.length; i++) {
+                List<List<Integer>> ttt = nSum2(nums, n - 1, i + 1, target - nums[i]);
+                for (int j = 0; j < ttt.size(); j++) {
+                    List<Integer> list = ttt.get(j);
+                    list.add(nums[i]);
+                    merged.add(list);
+                }
+                // 跳过第一个数字重复的情况，否则会出现重复结果
+                while (i < nums.length - 1 && nums[i] == nums[i + 1]) {
+                    i++;
+                }
+            }
+        }
+        return merged;
+    }
+}
+
